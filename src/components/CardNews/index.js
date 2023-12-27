08fc975e4c0e173a770cefa83bb75465ac0c993c
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-12-26 22:46:14 -0300

    build finalizado

--- src/components/CardNews/index.js
+++ src/components/CardNews/index.js
@@ -14,14 +14,32 @@ class CardNews extends HTMLElement {
         const cardLeft = document.createElement('div');
         cardLeft.setAttribute('class','card-left')
 
+        const autor = document.createElement('span');
+        autor.textContent = "By " + (this.getAttribute('autor') || 'Anonymous');
+
+        const linkNoticia = document.createElement('a');
+        linkNoticia.textContent = this.getAttribute('title');
+        linkNoticia.href = this.getAttribute('url') || "https://github.com/Carl-Vini?tab=repositories";
+
+        const paragrafo = document.createElement('p');
+        paragrafo.textContent = this.getAttribute('paragrafo');
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkNoticia);
+        cardLeft.appendChild(paragrafo);
+
+        const newsImage = document.createElement('img');
+        newsImage.src = this.getAttribute('imagem');
+        newsImage.alt = "foto do darth Vader";
         const cardRight = document.createElement('div');
-        cardRight.setAttribute('class','card-right')
+        cardRight.appendChild(newsImage);
+        cardRight.setAttribute('class','card-right');
         
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
 
 
-        return componentRoot
+        return componentRoot;
     }
 
     styles() {
